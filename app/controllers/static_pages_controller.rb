dfbec000d5a2b08053bd86c2c6736c89ed652817
dayne jones <jones.dayne@gmail.com>
2016-04-23 16:44:10 -0400

    fixing some thigns

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < Spree::HomeController
   def home
+    @searcher = build_searcher(params.merge(include_images: true))
+    @products = @searcher.retrieve_products
+    @taxonomies = Spree::Taxonomy.includes(root: :children)
   end
 
   def about
