25446d3ce74f64ac192330fdd37d428577527a32
Jordan Rhodes <jsrhodes15@gmail.com>
2016-04-29 16:59:01 -0400

    Edit image paths for carousel

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -36,7 +36,7 @@ Spree.config do |config|
   # Admin:
 
   # Custom logo for the admin
-    config.admin_interface_logo = "logo/solidus.png"
+    config.admin_interface_logo = "logo/solidus_logo.png"
 
   # Gateway credentials can be configured statically here and referenced from
   # the admin. They can also be fully configured from the admin.
